73bbff47361c0ac6ea327a746f14a6d2f69c6290
Erik Laats <zxzxzxko@scc2102-9.ucdavis.edu>
2018-02-06 11:50:23 -0800

    Studio2Updates

--- studio2/script.js
+++ studio2/script.js
@@ -21,23 +21,60 @@ var stuffbtn = document.getElementById('stuffbtn');
 var outfitbtn = document.getElementById('outfitbtn');
 var stuff = document.getElementById('stuff');
 var outfit = document.getElementById('outfit');
+var rocketbtn = document.getElementById('rocketbtn');
+var rocket = document.getElementById('rocket');
+var zoom = document.getElementById('zoom');
+var zoombtn = document.getElementById('zoombtn');
+var pixel = document.getElementById('overlayimg');
+
+var scroller = document.getElementById('scroller');
 
 stuffbtn.addEventListener('click', function() {
   stuff.style.display = "block";
   outfit.style.display = "none";
+  zoom.style.display = "none";
 });
 
 outfitbtn.addEventListener('click', function() {
   outfit.style.display = "block";
   stuff.style.display = "none";
+  zoom.style.display = "none";
 });
+
+rocketbtn.addEventListener('click', function() {
+  outfit.style.display = "none";
+  stuff.style.display = "none";
+  zoom.style.display = "block";
+});
+
+zoombtn.addEventListener('click', function() {
+  console.log('mouseoverzoom');
+  zoombtn.src = 'images/zoombright.png';
+
+  pirate.style.display = "none";
+  armor.style.display = "none";
+  workout.style.display = "none";
+  erik.style.display = "none";
+  rocket.style.display = "block";
+  scroller.style.animationDuration = '200s';
+  scroller.style.animationDuration = '20s';
+});
+
+zoombtn.addEventListener('mouseout', function() {
+  zoombtn.src = 'images/zoomdark.png';
+});
+
+
 
 piratebtn.addEventListener('click', function() {
 
   pirate.style.display = "block";
   armor.style.display = "none";
-    workout.style.display = "none";
-    erik.style.display = "none";
+  workout.style.display = "none";
+  erik.style.display = "none";
+  rocket.style.display = "none";
+
+  scroller.style.animationDuration = '200s';
 });
 
 armorbtn.addEventListener('click', function() {
@@ -45,6 +82,9 @@ armorbtn.addEventListener('click', function() {
   pirate.style.display = "none";
   workout.style.display = "none";
   erik.style.display = "none";
+  rocket.style.display = "none";
+
+  scroller.style.animationDuration = '200s';
 });
 
 workoutbtn.addEventListener('click', function() {
@@ -52,6 +92,9 @@ workoutbtn.addEventListener('click', function() {
   pirate.style.display = "none";
   workout.style.display = "block";
   erik.style.display = "none";
+  rocket.style.display = "none";
+
+  scroller.style.animationDuration = '200s';
 });
 
 erikbtn.addEventListener('click', function() {
@@ -59,23 +102,30 @@ erikbtn.addEventListener('click', function() {
   pirate.style.display = "none";
   workout.style.display = "none";
   erik.style.display = "block";
+
+  scroller.style.animationDuration = '200s';
 });
 
 
 waterbtn.addEventListener('click', function() {
+  pixel.src = 'images/waterbottle.png';
+
   overlay.style.display = "block";
   overlaytxt.innerHTML = "This is Erik's water bottle. It keeps him from getting dehydrated and grumpy.";
 });
 
 contbtn.addEventListener('click', function() {
+  pixel.src = 'images/controller.png';
   overlay.style.display = "block";
   overlaytxt.innerHTML = "This is Erik's GameCube controller. He uses it to play Super Smash Bros.";
 });
 lunchbtn.addEventListener('click', function() {
+  pixel.src = 'images/lunch.png';
   overlay.style.display = "block";
   overlaytxt.innerHTML = "This is Erik's lunch. It keeps him from getting hungry and grumpy.";
 });
 sketchbtn.addEventListener('click', function() {
+  pixel.src = 'images/sketchbook.png';
   overlay.style.display = "block";
   overlaytxt.innerHTML = "This is Erik's sketchbook. He carries it around to help masquerade as a 'creative person.'";
 });
