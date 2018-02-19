391d49e779009291ce27a39c58080c86e9871299
konopacz <edlaats@ucdavis.edu>
2018-02-18 20:15:20 -0800

    asdf

--- studio4/script.js
+++ studio4/script.js
@@ -36,14 +36,14 @@ function draw() {
         col3 = mezr.distance(character, enemt3);
 
   if (col1 < 0.001 || col2 < 0.001 || col3 < 0.001) {
-cSprite.src = "images/deadbowl.gif";
+
     character.style.animationPlayState = "paused";
     enemt.style.animationPlayState = "paused";
     enemt2.style.animationPlayState = "paused";
     enemt3.style.animationPlayState = "paused";
     canJump = false;
     canDuck = false;
-
+cSprite.src = "images/deadbowl.gif";
     setTimeout(gameReset, 2100);
 
   }
