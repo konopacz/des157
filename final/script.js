de21b247a81348602e37b26dc219c8201541f21d
konopacz <edlaats@ucdavis.edu>
2018-03-02 15:46:05 -0800

    Map Insanity

---
+++ final/script.js
@@ -0,0 +1,236 @@
+function initMap() {
+
+  // Create a new StyledMapType object, passing it an array of styles,
+  // and the name to be displayed on the map type control.
+  var styledMapType = new google.maps.StyledMapType(
+    [{
+        elementType: 'geometry',
+        stylers: [{
+          color: '#ebe3cd'
+        }]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'administrative',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#c9b2a6'
+        }]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#dcd2be'
+        }]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#ae9e90'
+        }]
+      },
+      {
+        featureType: 'landscape.natural',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#dfd2ae'
+        }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry.fill',
+        stylers: [{
+          color: '#a5b076'
+        }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#447530'
+        }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#f5f1e6'
+        }]
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#fdfcf8'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#f8c967'
+        }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#e9bc62'
+        }]
+      },
+      {
+        featureType: 'road.highway.controlled_access',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#e98d58'
+        }]
+      },
+      {
+        featureType: 'road.highway.controlled_access',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#db8555'
+        }]
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'labels',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'geometry',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry.fill',
+        stylers: [{
+          color: '#b9d3c2'
+        }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#92998d'
+        }]
+      },
+      {
+        featureType: 'all',
+        elementType: 'labels',
+        stylers: [{
+          visibility: 'off'
+        }]
+      },
+    ], {
+      name: 'Styled Map'
+    });
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 38.5547,
+      lng: -121.581
+    },
+    zoom: 15,
+    zoomControl: false,
+    scaleControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    gestureHandling: 'none',
+    disableDefaultUI: true,
+    mapTypeControlOptions: {
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+        'styled_map'
+      ]
+    }
+  });
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
+
+
+  // currentlocationstuff
+
+  var infoWindow;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+
+      map.setCenter(pos);
+      map.setZoom(20);
+
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+
+}
